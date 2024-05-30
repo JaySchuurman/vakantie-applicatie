2ec6d9c2be88c34dcf485b62cc0e8a05aa86ebfc
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2024-05-30 11:32:25 +0200

    test 2
    test 2 om te zien of hugo het nu ook ziet

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
-    <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet">
@@ -13,6 +13,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    test hoi mijn naam is john fortnite
+    <div class="test"></div>
 </body>
 </html>
